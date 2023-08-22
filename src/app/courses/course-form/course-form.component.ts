c0a7112a318bd2c481bd686266c79590acdc9edf
Anderson Andrade <andrade.28me@gmail.com>
2023-08-22 09:58:42 -0300

    service

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { SnackServiceService } from 'src/app/shared/services/snack-service.service';
 import { Location } from '@angular/common';
@@ -10,24 +10,25 @@ import { Location } from '@angular/common';
   styleUrls: ['./course-form.component.css']
 })
 export class CourseFormComponent {
+
   categorys = [
     { value: "front-end" },
     { value: "back-end" },
-    { value: null },
   ];
 
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
+
   constructor(
     private location: Location,
     private snackService: SnackServiceService,
     private service: CoursesService,
-    private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      name: [null],
-      category: [null]
-    });
+    private formBuilder: NonNullableFormBuilder) {
   }
+
   onSubmit() {
     this.service.save(this.form.value).subscribe(result => {
       this.location.back();
